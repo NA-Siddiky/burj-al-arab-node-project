88f1d9b404ddc77017b3ca4368df76d41e3d5d03
NA-Siddiky <siddiky.academic@gmail.com>
2021-03-26 03:45:45 +0600

    adding codes from others

--- src/components/Book/Book.js
+++ src/components/Book/Book.js
@@ -1,12 +1,44 @@
+import { Grid } from '@material-ui/core';
 import React from 'react';
 import { Link, useParams } from 'react-router-dom';
 
 const Book = () => {
-    const {bedType} = useParams();
+    const { bedType } = useParams();
     return (
-        <div style={{textAlign: 'center'}}>
+        <div style={{ textAlign: 'center' }}>
             <h1>Let's book a {bedType} Room.</h1>
             <p>Want a <Link to="/home">different room?</Link> </p>
+{/* 
+            <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                <Grid container justify="space-around">
+                    <KeyboardDatePicker
+                        disableToolbar
+                        variant="inline"
+                        format="MM/dd/yyyy"
+                        margin="normal"
+                        id="date-picker-inline"
+                        label="Date picker inline"
+                        value={selectedDate}
+                        onChange={handleDateChange}
+                        KeyboardButtonProps={{
+                            'aria-label': 'change date',
+                        }}
+                    />
+                    <KeyboardDatePicker
+                        margin="normal"
+                        id="date-picker-dialog"
+                        label="Date picker dialog"
+                        format="MM/dd/yyyy"
+                        value={selectedDate}
+                        onChange={handleDateChange}
+                        KeyboardButtonProps={{
+                            'aria-label': 'change date',
+                        }}
+                    />
+                </Grid>
+            </MuiPickersUtilsProvider> */}
+
+
         </div>
     );
 };
