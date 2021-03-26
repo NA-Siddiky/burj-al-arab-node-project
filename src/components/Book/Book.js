76e2fece975e1bb9bb0c6382c675b5390144c77c
NA-Siddiky <siddiky.academic@gmail.com>
2021-03-26 19:43:37 +0600

    adding new npm

--- src/components/Book/Book.js
+++ src/components/Book/Book.js
@@ -1,14 +1,29 @@
 import { Grid } from '@material-ui/core';
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link, useParams } from 'react-router-dom';
+
+import Grid from '@material-ui/core/Grid';
+import DateFnsUtils from '@date-io/date-fns';
+import {
+    MuiPickersUtilsProvider,
+    KeyboardDatePicker,
+} from '@material-ui/pickers';
+import { UserContext } from '../../App';
 
 const Book = () => {
     const { bedType } = useParams();
+    const [loggedInUser, setLoggedInUser] = useContext(UserContext);
+    const [selectedDate, setSelectedDate] = React.useState(new Date('2014-08-18T21:11:54'));
+
+    const handleDateChange = (date) => {
+        setSelectedDate(date);
+    };
+
     return (
         <div style={{ textAlign: 'center' }}>
             <h1>Let's book a {bedType} Room.</h1>
             <p>Want a <Link to="/home">different room?</Link> </p>
-{/* 
+
             <MuiPickersUtilsProvider utils={DateFnsUtils}>
                 <Grid container justify="space-around">
                     <KeyboardDatePicker
@@ -36,8 +51,7 @@ const Book = () => {
                         }}
                     />
                 </Grid>
-            </MuiPickersUtilsProvider> */}
-
+            </MuiPickersUtilsProvider>
 
         </div>
     );
