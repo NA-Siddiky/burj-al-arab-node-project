455b1ddf884f30c7f6b566c74d8d77db41683e64
NA-Siddiky <siddiky.academic@gmail.com>
2021-03-27 13:58:20 +0600

    verification

--- burj-al-arab-client/src/components/Bookings/Bookings.js
+++ burj-al-arab-client/src/components/Bookings/Bookings.js
@@ -4,9 +4,15 @@ import { UserContext } from '../../App';
 const Bookings = () => {
     const [bookings, setBookings] = useState([]);
     const [loggedInUser, setLoggedInUser] = useContext(UserContext);
-    
+
     useEffect(() => {
-        fetch('http://localhost:5000/bookings?email='+loggedInUser.email)
+        fetch('http://localhost:5000/bookings?email='+loggedInUser.email, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                authorization: `Bearer ${sessionStorage.getItem('token')}`
+            }
+        })
             .then(res => res.json())
             .then(data => setBookings(data));
     }, [])
--- burj-al-arab-server/index.js
+++ burj-al-arab-server/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const admin = require('firebase-admin');
 
 
 const port = 5000
@@ -8,6 +9,13 @@ const port = 5000
 const app = express()
 app.use(cors());
 app.use(bodyParser.json());
+
+var serviceAccount = require("./burj-al-arab-ccb7a-firebase-adminsdk-0xygq-140640b860.json");
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+
 
 const pass = "pust@2020";
 
@@ -29,11 +37,37 @@ client.connect(err => {
     })
 
     app.get('/bookings', (req, res) => {
-        // console.log(req.query.email);
-        bookings.find({ email: req.query.email })
-            .toArray((err, documents) => {
-                res.send(documents)
-            })
+        const bearer = req.headers.authorization
+        if (bearer && bearer.startsWith('Bearer ')) {
+            const idToken = bearer.split(' ')[1];
+            console.log({ idToken });
+
+            // idToken comes from the client app
+            admin
+                .auth()
+                .verifyIdToken(idToken)
+                .then((decodedToken) => {
+                    const tokenEmail = decodedToken.email;
+                    const queryEmail = req.query.email;
+                    console.log(tokenEmail, queryEmail);
+                    if (tokenEmail == req.query.email) {
+                        bookings.find({ email: req.query.email })
+                            .toArray((err, documents) => {
+                                res.send(documents);
+                            })
+                    }
+
+                })
+                .catch((error) => {
+                    // Handle error
+                });
+
+        }
+
+        // bookings.find({ email: req.query.email })
+        //     .toArray((err, documents) => {
+        //         res.send(documents)
+        //     })
     })
 
 });
