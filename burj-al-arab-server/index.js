383b441a0533b0622ddf5347c9ff3d46aff36f68
NA-Siddiky <siddiky.academic@gmail.com>
2021-03-27 15:04:25 +0600

    complete code

--- burj-al-arab-server/index.js
+++ burj-al-arab-server/index.js
@@ -2,25 +2,24 @@ const express = require('express')
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const admin = require('firebase-admin');
+const MongoClient = require('mongodb').MongoClient;
+require('dotenv').config()
 
-
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ylija.mongodb.net/BurjAlArab?retryWrites=true&w=majority`;
 const port = 5000
 
 const app = express()
 app.use(cors());
 app.use(bodyParser.json());
 
-var serviceAccount = require("./burj-al-arab-ccb7a-firebase-adminsdk-0xygq-140640b860.json");
+var serviceAccount = require("./configs/burj-al-arab-ccb7a-firebase-adminsdk-0xygq-140640b860.json");
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
+    // databaseURL : process.env.FIRE_DB
 });
 
 
-const pass = "pust@2020";
-
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://alarab:pust@2020@cluster0.ylija.mongodb.net/BurjAlArab?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
     const bookings = client.db("BurjAlArab").collection("bookings");
@@ -40,7 +39,7 @@ client.connect(err => {
         const bearer = req.headers.authorization
         if (bearer && bearer.startsWith('Bearer ')) {
             const idToken = bearer.split(' ')[1];
-            console.log({ idToken });
+            // console.log({ idToken });
 
             // idToken comes from the client app
             admin
@@ -49,19 +48,26 @@ client.connect(err => {
                 .then((decodedToken) => {
                     const tokenEmail = decodedToken.email;
                     const queryEmail = req.query.email;
-                    console.log(tokenEmail, queryEmail);
-                    if (tokenEmail == req.query.email) {
-                        bookings.find({ email: req.query.email })
+                    // console.log(tokenEmail, queryEmail);
+                    if (tokenEmail == queryEmail) {
+                        bookings.find({ email: queryEmail })
                             .toArray((err, documents) => {
-                                res.send(documents);
+                                res.status(200).send(documents);
                             })
+                    }
+                    else {
+                        res.status(401).send('Users not Found');
                     }
 
                 })
                 .catch((error) => {
-                    // Handle error
+                    res.status(401).send('Wrong Token');
+
                 });
 
+        }
+        else {
+            res.status(401).send('unauthorized access');
         }
 
         // bookings.find({ email: req.query.email })
